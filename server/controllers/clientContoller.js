16225d1ac3d777cf2fb3c8d40f74a80491f9befe
Egor <moskvinegor2007@gmail.com>
2026-03-19 12:04:50 +0300

    new

--- server/controllers/clientContoller.js
+++ server/controllers/clientContoller.js
@@ -33,9 +33,38 @@ export const getOneClient= async  (req, res,next) => {
         res.status(404).json({
             message:'Пользователь с таким ID не найден'
         })
-        res.status(200).json(getAllClients)
+        res.status(200).json(oneclient)
     }
     catch(err){
         next(err)
     }
+}
+//HTTP-PUT метод
+export const updateClient = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const [updated] = await Client.update(req.body, { where: { id } })
+    if (!updated) return res.status(404).json({message: 'Клиент не найден'})
+      const client = await Client.findByPk(id)
+      res.json(client)
+  }
+  catch (err) {
+    next(err)
+  }
+}
+//HTTP-PATCH метод
+export const updateClientStatus = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { rating } = req.body
+ 
+    const newRating = await Client.findByPk(id)
+    if (!newRating) return res.status(404).json({ message: "Клиент не найден"})
+      newRating.rating = rating
+    await newRating.save()
+    res.json(newRating)
+  }
+   catch (err) {
+    next(err)
+  }
 }
